eca25e43476af088be8ae0fefeeaaaee0bc3ac55
Hardik Kaji <hardik.kaji@netent.com>
2019-03-21 12:04:05 +0100

    Fix build

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'build')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
